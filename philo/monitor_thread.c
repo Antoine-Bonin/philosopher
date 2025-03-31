1150b2f70dbfa9559cca2b69f094ee07c0f0ac0d
antoine <antoine.bonin78@gmail.com>
2025-03-31 15:51:59 +0200

    refactor: separate death checking logic from monitor routine for clarity + modifyedd implementation of meal_eat at the start of the routine for better logic

--- philo/monitor_thread.c
+++ philo/monitor_thread.c
@@ -6,7 +6,7 @@
 /*   By: antbonin <antbonin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 16:36:35 by antbonin          #+#    #+#             */
-/*   Updated: 2025/03/27 14:34:15 by antbonin         ###   ########.fr       */
+/*   Updated: 2025/03/31 15:51:09 by antbonin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,40 +32,45 @@ void	philo_ate(t_data *data)
 	pthread_mutex_unlock(&data->is_dead);
 }
 
-void *monitor_routine(void *arg)
+void	check_death(t_data *data, int *i, int *ate)
 {
-    t_data  *data;
-    long    current_time;
-    int     i;
-    int     ate;
+	long	current_time;
 
-    data = (t_data *)arg;
-    while (!should_stop(data))
-    {
-        i = 0;
-        ate = 0;
-        while (i < data->nb_philo && !data->stop)
-        {
-            current_time = get_current_time();
-            pthread_mutex_lock(&data->update);
-            if ((current_time + 1)- data->philos[i].last_meal > data->time_to_die) 
-            {
-                death_occured(data, i);
-                return (NULL);
-            }
-            if (data->nb_eat != -1 && data->philos[i].meal_eat >= data->nb_eat- 1)
-                ate++;    
-            pthread_mutex_unlock(&data->update);
-            i++;
-        }
-        if (data->nb_eat != -1 && ate == data->nb_philo)
-        {
-            pthread_mutex_lock(&data->update);
-            philo_ate(data);
-            return (NULL);
-        }
-        
-        usleep(1000);
-    }
-    return (NULL);
+	pthread_mutex_lock(&data->update);
+	current_time = get_current_time();
+	if (current_time - data->philos[*i].last_meal > data->time_to_die)
+	{
+		death_occured(data, *i);
+		return ;
+	}
+	if (data->nb_eat != -1 && data->philos[*i].meal_eat >= data->nb_eat)
+		(*ate)++;
+	pthread_mutex_unlock(&data->update);
+}
+
+void	*monitor_routine(void *arg)
+{
+	t_data	*data;
+	int		i;
+	int		ate;
+
+	data = (t_data *)arg;
+	usleep(1000);
+	while (!should_stop(data))
+	{
+		i = 0;
+		ate = 0;
+		while (i < data->nb_philo && !should_stop(data))
+		{
+			check_death(data, &i, &ate);
+			i++;
+		}
+		if (data->nb_eat != -1 && ate == data->nb_philo)
+		{
+			pthread_mutex_lock(&data->update);
+			return (philo_ate(data), NULL);
+		}
+		usleep(1000);
+	}
+	return (NULL);
 }
